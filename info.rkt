d5b7ba438ccdead8213e96051a205b696e4a8a93
belph <peblairman@gmail.com>
2015-07-10 11:26:31 -0400

    Fixing dependency problems

--- info.rkt
+++ info.rkt
@@ -1,5 +1,7 @@
 #lang info
 (define collection "c-defs")
 (define compile-omit-paths (list "examples"))
-(define version "1.0")
+(define version "1.0.1")
 (define scribblings '(("docs/c-defs.scrbl")))
+(define deps '("base"))
+(define build-deps '("sandbox-lib" "scribble-lib"))
